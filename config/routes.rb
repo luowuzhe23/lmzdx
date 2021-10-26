ffaba6b2b09ddd098ce1f8a699c226423005c21c
shengchao <ishengchao@outlook.com>
2021-10-26 15:26:42 +0800

    脚手架生成post;手动创建comment相关

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
+  resources :posts do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
